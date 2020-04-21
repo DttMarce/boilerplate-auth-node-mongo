9dcbbfb51e2c22c2371efb419e0b3256cf4b4cae
Marce <mgalindodev@gmail.com>
2020-04-21 19:12:40 +0200

    Add CORS

--- config/express.js
+++ config/express.js
@@ -4,15 +4,13 @@ const celebrate = require('celebrate');
 const cors = require('cors');
 
 const api = require('../api/routes');
-const accessControl = require('../config/middleware/access-control');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(accessControl);
-app.use('/v1/', api);
+app.use('/api/', api);
 app.use(celebrate.errors());
 
 module.exports = app;
